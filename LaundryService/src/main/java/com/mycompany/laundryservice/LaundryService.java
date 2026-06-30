a8b7772a9a29519b267c37505e4c80d71a5bb61a
Hughchie <abantehughcshsb@gmail.com>
2026-06-30 18:34:19 +0800

    Restore LaundryService files

---
+++ LaundryService/src/main/java/com/mycompany/laundryservice/LaundryService.java
@@ -0,0 +1,8 @@
+package com.mycompany.laundryservice;
+
+public class LaundryService {
+
+    public static void main(String[] args) {
+	    System.out.println("com.mycompany.laundryservice.LaundryService.main()");
+    }
+}
