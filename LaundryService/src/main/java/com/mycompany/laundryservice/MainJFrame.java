1b6416d5c6179b3689bd131aaa303807d9d650c7
jairus634 <jairusrivero3@gmail.com>
2026-07-03 01:01:55 +0800

    Add New

--- LaundryService/src/main/java/com/mycompany/laundryservice/MainJFrame.java
+++ LaundryService/src/main/java/com/mycompany/laundryservice/MainJFrame.java
@@ -8,6 +8,8 @@ package com.mycompany.laundryservice;
  *
  * @author Cral
  */
+import com.formdev.flatlaf.FlatLightLaf;
+
 public class MainJFrame extends javax.swing.JFrame {
 	
 	private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(MainJFrame.class.getName());
@@ -50,6 +52,7 @@ public class MainJFrame extends javax.swing.JFrame {
 	 */
 	public static void main(String args[]) {
 		/* Set the Nimbus look and feel */
+                 FlatLightLaf.setup();
 		//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
 		/* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
